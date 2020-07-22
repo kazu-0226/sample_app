fd4d49f246a0936b7ab0481da4387e96d88b0ea7
vagrant <vagrant@localhost.localdomain>
2020-07-22 09:07:47 +0000

    [Add]dotenv-rails

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'd21caf38f6b199d4dbc56ced4425a63ce044f2b265e22d932996891d9dd5a675ece0f5e7b7752ba4143d1ef1f1fe98a9fde98598701eb7f7926f1049b70b018d'
